ce42ef2d80d36263eb681a47f9d03ee1f9547839
iJoise <kondratevk.1989@mail.ru>
2021-08-19 00:16:15 +0700

    implemented Title and Profile page

---
+++ src/components/Title.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledTitle = styled.h1`
+  font-weight: bold;
+  font-size: 64px;
+  line-height: 78px;
+  margin-top: 40px;
+`
+
+export const Title: React.FC = () => {
+
+   return <StyledTitle>
+         ONLY.
+      </StyledTitle>
+
+}
