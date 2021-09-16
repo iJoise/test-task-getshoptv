35a2bcd35f616e7165829250787ca44ad1bcdc23
Kirill-Kondratev <kk@x-lab.com.ru>
2021-09-16 16:23:57 +0700

    change

--- dir-B/src/state/action.ts
+++
@@ -1,8 +0,0 @@
-import {Action_Type} from "../constants";
-
-export const action = {
-   setIsFetching: (isFetching: boolean) => ({type: Action_Type.SET_IS_FETCHING, payload: {isFetching}}),
-   setIsError: (isError: string) => ({type: Action_Type.SET_IS_ERROR, payload: {isError}}),
-   setUserEmail: (userEmail: string) => ({type: Action_Type.SET_USER_NAME, payload: {userEmail}}),
-   setIsAuth: (isAuth: boolean) => ({type: Action_Type.SET_IS_AUTH, payload: {isAuth}})
-}
